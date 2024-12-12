2f14c00f7d3314379ec25f2226a07048b6994efa
darkwhole33 <abhishekadhav04@gmail.com>
2024-12-12 17:18:18 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -147,7 +147,7 @@ Here is the help for my COMMANDS."""
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/LazyDeveloper)</code>
+<code>[Button Text](buttonurl:https://t.me/akingediti99)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
